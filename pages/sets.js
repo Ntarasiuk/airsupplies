c7f16d685f3b377174f5aacef83c558a9eff29d2
Nathan Tarasiuk <ntarasiuk@gmail.com>
2021-09-28 16:55:48 -0700

    query set by category

--- pages/sets.js
+++ pages/sets.js
@@ -10,23 +10,23 @@ function SetPage() {
 
   const { data, loading, error } = useQuery(gql`
     {
-      set {
-        id
-        cover_image
+      category {
+        slug
         description
-        subtitle
-        title
-        is_airbnb_plus
-        category {
-          slug
+        name
+        sets {
+          id
+          cover_image
           description
-          name
+          subtitle
+          title
+          is_airbnb_plus
         }
       }
     }
   `);
 
-  const setData = data?.set || [];
+  const catData = data?.category || [];
   if (loading)
     return (
       <Layout>
@@ -35,33 +35,37 @@ function SetPage() {
     );
   return (
     <Layout>
-      <h4>Bathroom Supplies</h4>
-      <hr />
-      <Grid.Container gap={2}>
-        {setData?.map((set) => (
-          <Grid xs={24} md={12} lg={8} xl={6} key={set?.id}>
-            <SetCard
-              image={set?.cover_image}
-              title={set?.title}
-              content={set?.subtitle}
-              btnText="Check out the list"
-              stars={5}
-              link={`/sets/${set?.category.slug}/${set?.id}`}
-              showStars={false}
-              badge={
-                set?.is_airbnb_plus ? (
-                  <>
-                    <Spacer w={0.5} />
-                    <Badge style={{ backgroundColor: theme.palette.alert }}>
-                      Airbnb Plus
-                    </Badge>
-                  </>
-                ) : null
-              }
-            />
-          </Grid>
-        ))}
-      </Grid.Container>
+      {catData?.map((cat) => (
+        <div key={cat?.id}>
+          {cat?.name ? <h4>{cat.name} Supplies</h4> : null}
+          <hr />
+          <Grid.Container gap={2}>
+            {[...cat?.sets]?.map((set) => (
+              <Grid xs={24} md={12} lg={8} xl={6} key={set?.id}>
+                <SetCard
+                  image={set?.cover_image}
+                  title={set?.title}
+                  content={set?.subtitle}
+                  btnText="Check out the list"
+                  stars={5}
+                  link={`/sets/${cat?.slug}/${set?.id}`}
+                  showStars={false}
+                  badge={
+                    set?.is_airbnb_plus ? (
+                      <>
+                        <Spacer w={0.5} />
+                        <Badge style={{ backgroundColor: theme.palette.alert }}>
+                          Airbnb Plus
+                        </Badge>
+                      </>
+                    ) : null
+                  }
+                />
+              </Grid>
+            ))}
+          </Grid.Container>
+        </div>
+      ))}
     </Layout>
   );
 }
