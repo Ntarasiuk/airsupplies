15f08a3240346631e4f76680510e80d295eb481f
Nathan Tarasiuk <ntarasiuk@gmail.com>
2021-10-11 19:12:41 -0700

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
 import { ApolloProvider } from "@apollo/client";
 import { CssBaseline, GeistProvider } from "@geist-ui/react";
 import "inter-ui/inter.css";
+import Script from "next/script";
 import { client } from "utils/apollo";
 
 const Application = ({ Component, pageProps }) => (
-  <ApolloProvider client={client}>
-    <GeistProvider>
-      <CssBaseline />
-      <Component {...pageProps} />{" "}
-    </GeistProvider>
-  </ApolloProvider>
+  <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="lazyOnload"
+      src={`https://www.googletagmanager.com/gtag/js?id=G-9QPVDZHCDT`}
+    />
+
+    <Script strategy="lazyOnload" id="gtag-config">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-9QPVDZHCDT');
+  `}
+    </Script>
+    <ApolloProvider client={client}>
+      <GeistProvider>
+        <CssBaseline />
+        <Component {...pageProps} />{" "}
+      </GeistProvider>
+    </ApolloProvider>
+  </>
 );
 
 export default Application;
