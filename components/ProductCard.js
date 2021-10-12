964f146ef6a839600a38ccfcc825d050783fffc4
Nathan Tarasiuk <ntarasiuk@gmail.com>
2021-10-11 19:16:46 -0700

    update link

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,7 +1,8 @@
-import { Card, Image, Link, Text, useMediaQuery } from "@geist-ui/react";
+import { Card, Image, Link, Text } from "@geist-ui/react";
+import NextLink from "next/link";
 import React from "react";
 
-function ProductCard({
+const ProductCard = ({
   width = "400px",
   height = "200px",
   image,
@@ -9,29 +10,28 @@ function ProductCard({
   description,
   title,
   subtitle,
-}) {
-  const isXS = useMediaQuery("xs");
-  const isSM = useMediaQuery("sm");
-
-  return (
-    <Card width="100%">
-      <Image src={image} height={height} alt={title} draggable={false} />
-      <Text h4 mb={0}>
-        {title}
-      </Text>
-      <Text type="secondary" small>
-        {subtitle}
-        {description ? (
-          <div dangerouslySetInnerHTML={{ __html: description }} />
-        ) : null}
-      </Text>
-      <Card.Footer>
-        <Link block target="_blank" href={`${url}?tag=airbundles-20`}>
-          Details
-        </Link>
-      </Card.Footer>
-    </Card>
-  );
-}
+}) => (
+  <NextLink target="_blank" href={url}>
+    <Link style={{ width: "100%" }}>
+      <Card width="100%">
+        <Image src={image} height={height} alt={title} draggable={false} />
+        <Text h4 mb={0}>
+          {title}
+        </Text>
+        <Text type="secondary" small>
+          {subtitle}
+          {description ? (
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+          ) : null}
+        </Text>
+        <Card.Footer>
+          <Link block target="_blank" href={`${url}?tag=airbundles-20`}>
+            Details
+          </Link>
+        </Card.Footer>
+      </Card>
+    </Link>
+  </NextLink>
+);
 
 export default ProductCard;
